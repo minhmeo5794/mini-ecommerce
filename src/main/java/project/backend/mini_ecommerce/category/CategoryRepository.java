c1d4b871dd2b5fbb50280195537776673019b066
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-05 18:33:28 +0700

    Add category repository

---
+++ src/main/java/project/backend/mini_ecommerce/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package project.backend.mini_ecommerce.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
