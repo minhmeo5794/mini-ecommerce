9d3de1a6123eedc211070305b3374b5a3d8f12b6
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-06 23:02:25 +0700

    feat: Add toEntity mapper

--- src/main/java/project/backend/mini_ecommerce/category/CategoryMapper.java
+++ src/main/java/project/backend/mini_ecommerce/category/CategoryMapper.java
@@ -2,6 +2,7 @@ package project.backend.mini_ecommerce.category;
 
 import org.springframework.stereotype.Component;
 import project.backend.mini_ecommerce.category.dto.CategoryResponse;
+import project.backend.mini_ecommerce.category.dto.CreateCategoryRequest;
 
 @Component
 public class CategoryMapper {
@@ -10,6 +11,13 @@ public class CategoryMapper {
                 .id(category.getId())
                 .name(category.getName())
                 .description(category.getDescription())
+                .build();
+    }
+
+    public Category toEntity(CreateCategoryRequest request) {
+        return Category.builder()
+                .name(request.getName().trim())
+                .description(request.getDescription().trim())
                 .build();
     }
 }
