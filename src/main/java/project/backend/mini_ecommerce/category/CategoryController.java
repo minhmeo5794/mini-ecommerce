1ed28c9f0792e2df1a37b181471b152c6841479d
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-07 17:48:49 +0700

    fix: Change response from plain to ApiResponse

--- src/main/java/project/backend/mini_ecommerce/category/CategoryController.java
+++ src/main/java/project/backend/mini_ecommerce/category/CategoryController.java
@@ -1,12 +1,17 @@
 package project.backend.mini_ecommerce.category;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.backend.mini_ecommerce.category.dto.CategoryResponse;
 import project.backend.mini_ecommerce.category.dto.CreateCategoryRequest;
-
-import java.util.List;
+import project.backend.mini_ecommerce.common.response.ApiResponse;
+import project.backend.mini_ecommerce.common.response.PageResponse;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/categories")
@@ -15,17 +20,38 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<CategoryResponse> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<ApiResponse<PageResponse<CategoryResponse>>> getAllCategories(
+            HttpServletRequest httpServletRequest,
+            @PageableDefault(page = 0, size = 5) Pageable pageable
+    ) {
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        categoryService.getAllCategories(pageable),
+                        "Get all categories successfully",
+                        HttpStatus.OK.value(),
+                        httpServletRequest
+                ));
     }
 
     @GetMapping("/{categoryId}")
-    public CategoryResponse getCategory(@PathVariable("categoryId") Long id) {
-        return categoryService.getCategory(id);
+    public ResponseEntity<ApiResponse<CategoryResponse>> getCategory(@PathVariable("categoryId") Long id, HttpServletRequest httpServletRequest) {
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        categoryService.getCategory(id),
+                        "Get category successfully",
+                        HttpStatus.OK.value(),
+                        httpServletRequest
+                ));
     }
 
     @PostMapping
-    public CategoryResponse createCategory(@RequestBody @Valid CreateCategoryRequest request) {
-        return categoryService.createCategory(request);
+    public ResponseEntity<ApiResponse<CategoryResponse>> createCategory(@RequestBody @Valid CreateCategoryRequest request, HttpServletRequest httpServletRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.success(
+                        categoryService.createCategory(request),
+                        "Category created successfully",
+                        HttpStatus.CREATED.value(),
+                        httpServletRequest
+                ));
     }
 }
