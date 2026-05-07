3980135ba9e3b86f3d88481813e1896c6f280977
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-07 17:46:16 +0700

    feat: Add ApiResponse file

--- src/main/java/project/backend/mini_ecommerce/common/response/ApiResponse.java
+++ src/main/java/project/backend/mini_ecommerce/common/response/ApiResponse.java
@@ -1,5 +1,6 @@
 package project.backend.mini_ecommerce.common.response;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,11 +10,21 @@ import java.time.Instant;
 @Getter
 public class ApiResponse<T> {
     private final T data;
-    private final String message;
     private final boolean success;
+    private final String message;
     private final String path;
     private final int status;
 
     @Builder.Default
     private final Instant timestamp = Instant.now();
+
+    public static <T> ApiResponse<T> success(T data, String message, int status, HttpServletRequest httpServletRequest) {
+        return ApiResponse.<T>builder()
+                .data(data)
+                .success(true)
+                .message(message)
+                .path(httpServletRequest.getRequestURI())
+                .status(status)
+                .build();
+    }
 }
