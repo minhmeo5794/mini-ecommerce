51dc2c76cb4c4b713b93cd6bd3503b67dcf0a6b7
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-08 11:01:02 +0700

    refactor: change plain response to api response

--- src/main/java/project/backend/mini_ecommerce/product/AdminProductController.java
+++ src/main/java/project/backend/mini_ecommerce/product/AdminProductController.java
@@ -1,9 +1,12 @@
 package project.backend.mini_ecommerce.product;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import project.backend.mini_ecommerce.common.response.ApiResponse;
 import project.backend.mini_ecommerce.product.dto.CreateProductRequest;
 import project.backend.mini_ecommerce.product.dto.ProductResponse;
 import project.backend.mini_ecommerce.product.dto.UpdateProductRequest;
@@ -16,18 +19,39 @@ public class AdminProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ProductResponse createProduct(@RequestBody @Valid CreateProductRequest request) {
-        return productService.createProduct(request);
+    public ResponseEntity<ApiResponse<ProductResponse>> createProduct(@RequestBody @Valid CreateProductRequest request, HttpServletRequest httpServletRequest) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.success(
+                        productService.createProduct(request),
+                        "Product created successfully",
+                        HttpStatus.CREATED.value(),
+                        httpServletRequest
+                ));
     }
 
     @PatchMapping("/{productId}")
-    public ProductResponse updateProduct(@PathVariable("productId") Long id, @RequestBody @Valid UpdateProductRequest request) {
-        return productService.updateProduct(id, request);
+    public ResponseEntity<ApiResponse<ProductResponse>> updateProduct(@PathVariable("productId") Long id, @RequestBody @Valid UpdateProductRequest request, HttpServletRequest httpServletRequest) {
+
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        productService.updateProduct(id, request),
+                        "Product updated successfully",
+                        HttpStatus.OK.value(),
+                        httpServletRequest
+                ));
     }
 
     @DeleteMapping("/{productId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable("productId") Long id) {
+    public ResponseEntity<ApiResponse<ProductResponse>> deleteProduct(@PathVariable("productId") Long id, HttpServletRequest httpServletRequest) {
         productService.deleteProduct(id);
-        return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        null,
+                        "Product deleted successfully",
+                        HttpStatus.OK.value(),
+                        httpServletRequest
+                ));
     }
 }
