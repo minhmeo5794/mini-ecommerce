fd5fdb51054e60e700c10f9b7cd17c172176b0c5
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-14 16:47:37 +0700

    feat: add toEntity method

--- src/main/java/project/backend/mini_ecommerce/user/UserMapper.java
+++ src/main/java/project/backend/mini_ecommerce/user/UserMapper.java
@@ -1,10 +1,20 @@
 package project.backend.mini_ecommerce.user;
 
 import org.springframework.stereotype.Component;
+import project.backend.mini_ecommerce.user.dto.CreateUserRequest;
 import project.backend.mini_ecommerce.user.dto.UserResponse;
 
 @Component
 public class UserMapper {
+    public User toEntity(CreateUserRequest request, String encodedPassword) {
+        return User.builder()
+                .email(request.getEmail())
+                .fullName(request.getFullName())
+                .phoneNumber(request.getPhoneNumber())
+                .password(encodedPassword)
+                .build();
+    }
+
     public UserResponse toResponse(User user) {
         return UserResponse.builder()
                 .id(user.getId())
