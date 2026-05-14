3e399c720e309f4484d580c40a9be07f35f3a4d3
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-14 16:00:15 +0700

    refactor: add api response format

--- src/main/java/project/backend/mini_ecommerce/auth/AuthController.java
+++ src/main/java/project/backend/mini_ecommerce/auth/AuthController.java
@@ -1,12 +1,16 @@
 package project.backend.mini_ecommerce.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.backend.mini_ecommerce.auth.dto.LoginRequest;
 import project.backend.mini_ecommerce.auth.dto.RegisterRequest;
 import project.backend.mini_ecommerce.auth.dto.LoginResponse;
 import project.backend.mini_ecommerce.auth.dto.RegisterResponse;
+import project.backend.mini_ecommerce.common.response.ApiResponse;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,12 +19,26 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public RegisterResponse register(@RequestBody @Valid RegisterRequest request) {
-        return authService.register(request);
+    public ResponseEntity<ApiResponse<RegisterResponse>> register(@RequestBody @Valid RegisterRequest request, HttpServletRequest httpServletRequest) {
+
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        authService.register(request),
+                        "User registered successfully",
+                        HttpStatus.OK.value(),
+                        httpServletRequest
+                ));
     }
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody @Valid LoginRequest request) {
-        return authService.login(request);
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@RequestBody @Valid LoginRequest request, HttpServletRequest httpServletRequest) {
+
+        return ResponseEntity.ok().body(
+                ApiResponse.success(
+                        authService.login(request),
+                        "User logged in successfully",
+                        HttpStatus.OK.value(),
+                        httpServletRequest
+                ));
     }
 }
