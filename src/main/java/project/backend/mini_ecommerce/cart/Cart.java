39156e2805664bb90a32a8149edc3c171a4c51b9
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-18 23:54:56 +0700

    feat: add cart entity

---
+++ src/main/java/project/backend/mini_ecommerce/cart/Cart.java
@@ -0,0 +1,34 @@
+package project.backend.mini_ecommerce.cart;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import project.backend.mini_ecommerce.user.User;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "carts")
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
