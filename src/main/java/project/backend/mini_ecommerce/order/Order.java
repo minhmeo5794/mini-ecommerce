abd19375f4a8b245da0a690086bd6c5b362aa627
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-20 12:08:08 +0700

    feat: add Order entity

---
+++ src/main/java/project/backend/mini_ecommerce/order/Order.java
@@ -0,0 +1,47 @@
+package project.backend.mini_ecommerce.order;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import project.backend.mini_ecommerce.common.enums.OrderStatus;
+import project.backend.mini_ecommerce.user.User;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private OrderStatus status = OrderStatus.PENDING;
+
+    @Column(nullable = false)
+    private String shippingAddress;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
