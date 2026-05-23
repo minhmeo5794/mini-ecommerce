6590b519fc1e81dbfee6b99d1aaf5a651f712522
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-23 18:30:41 +0700

    fix: add fetch lazy

--- src/main/java/project/backend/mini_ecommerce/order/Order.java
+++ src/main/java/project/backend/mini_ecommerce/order/Order.java
@@ -22,7 +22,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany
+    @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
