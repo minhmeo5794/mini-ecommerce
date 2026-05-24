b66f2d17fd3ce7d55cb1b0fe4e3ea76e896a0a76
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-25 04:05:09 +0700

    feat: add CartResponse

---
+++ src/main/java/project/backend/mini_ecommerce/cart/dto/CartResponse.java
@@ -0,0 +1,17 @@
+package project.backend.mini_ecommerce.cart.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import project.backend.mini_ecommerce.cart_item.dto.CartItemResponse;
+import project.backend.mini_ecommerce.common.response.PageResponse;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+public class CartResponse {
+    private final Long id;
+    private final Long userId;
+    private final BigDecimal totalAmount;
+    private final PageResponse<CartItemResponse> cartItems;
+}
