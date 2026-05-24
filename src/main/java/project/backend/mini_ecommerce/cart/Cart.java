6177d5920c2dcbeab05195a02b17a7376adf4e9e
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-25 01:59:44 +0700

    feat: add cartItems field

--- src/main/java/project/backend/mini_ecommerce/cart/Cart.java
+++ src/main/java/project/backend/mini_ecommerce/cart/Cart.java
@@ -4,9 +4,11 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import project.backend.mini_ecommerce.cart_item.CartItem;
 import project.backend.mini_ecommerce.user.User;
 
 import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,6 +25,9 @@ public class Cart {
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "cart") // mappedBy
+    private List<CartItem> cartItems;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
