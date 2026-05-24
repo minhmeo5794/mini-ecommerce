4fa131d3497347094e0e3b521e8ba15786e2105d
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-24 23:42:03 +0700

    fix: association logic

--- src/main/java/project/backend/mini_ecommerce/order/Order.java
+++ src/main/java/project/backend/mini_ecommerce/order/Order.java
@@ -22,7 +22,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
