ec4c598d4c60453acacc3a422b6aab451f2be17c
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-25 15:55:49 +0700

    fix: change findByCartId from Page to List

--- src/main/java/project/backend/mini_ecommerce/cart_item/CartItemRepository.java
+++ src/main/java/project/backend/mini_ecommerce/cart_item/CartItemRepository.java
@@ -1,11 +1,11 @@
 package project.backend.mini_ecommerce.cart_item;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Page<CartItem> findByCartId(Long cartId, Pageable pageable);
+    List<CartItem> findByCartId(Long cartId);
 }
