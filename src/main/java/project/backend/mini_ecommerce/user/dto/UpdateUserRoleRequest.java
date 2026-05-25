df07ec969b34afe915f3e2a2ebbc097b02dacd09
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-25 16:12:46 +0700

    fix: using RequiredCons instead of Builder

--- src/main/java/project/backend/mini_ecommerce/user/dto/UpdateUserRoleRequest.java
+++ src/main/java/project/backend/mini_ecommerce/user/dto/UpdateUserRoleRequest.java
@@ -1,12 +1,12 @@
 package project.backend.mini_ecommerce.user.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import project.backend.mini_ecommerce.common.enums.UserRole;
 
 @Getter
-@Builder
+@RequiredArgsConstructor
 public class UpdateUserRoleRequest {
     @NotNull(message = "Role can not be null")
     private final UserRole role;
--- src/main/java/project/backend/mini_ecommerce/user/dto/UpdateUserStatusRequest.java
+++ src/main/java/project/backend/mini_ecommerce/user/dto/UpdateUserStatusRequest.java
@@ -1,12 +1,12 @@
 package project.backend.mini_ecommerce.user.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import project.backend.mini_ecommerce.common.enums.UserStatus;
 
 @Getter
-@Builder
+@RequiredArgsConstructor
 public class UpdateUserStatusRequest {
     @NotNull(message = "Status can not be null")
     private final UserStatus status;
