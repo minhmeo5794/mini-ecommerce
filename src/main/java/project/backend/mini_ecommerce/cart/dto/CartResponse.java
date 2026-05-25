ddc6da6f5ed05b96da5278f170378bb0b4321d92
minhmeo5794 <vuquangminh0507@gmail.com>
2026-05-25 15:54:38 +0700

    fix: change cartItems from Page to List

--- src/main/java/project/backend/mini_ecommerce/cart/dto/CartResponse.java
+++ src/main/java/project/backend/mini_ecommerce/cart/dto/CartResponse.java
@@ -3,9 +3,9 @@ package project.backend.mini_ecommerce.cart.dto;
 import lombok.Builder;
 import lombok.Getter;
 import project.backend.mini_ecommerce.cart_item.dto.CartItemResponse;
-import project.backend.mini_ecommerce.common.response.PageResponse;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Builder
@@ -14,5 +14,5 @@ public class CartResponse {
     private final Long userId;
     private final BigDecimal totalAmount;
     private final Integer totalItems;
-    private final PageResponse<CartItemResponse> cartItems;
+    private final List<CartItemResponse> cartItems;
 }
